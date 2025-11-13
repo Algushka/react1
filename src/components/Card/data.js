d71e5e30803bc20566d716f9e18b6519fe268ed7
Algushka <successolga118@gmail.com>
2025-11-13 20:09:10 +0300

    information

--- src/components/Card/data.js
+++ src/components/Card/data.js
@@ -1,7 +1,7 @@
 const card = {
   Name: "Homer",
   Surname: "Simpson",
-  Profession: "sitting at home and drinking beer",
+  Profession: "Sitting at home",
   Hobby: "Drink beer",
   avatarURL:
     "https://funny.grizly.club/uploads/posts/2024-12/funny-grizly-club-zpjq-p-memi-gomer-simpson-12.jpg",
