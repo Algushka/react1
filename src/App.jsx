3bc18d2d5f87fe3b3d9eaa044f49a0f0b0220c88
Algushka <successolga118@gmail.com>
2025-11-20 20:25:10 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
 import Lesson_03 from "./lessons/Lesson_03/Lesson_03";
 import Lesson_04 from "./lessons/Lesson_04/Lesson_04"
 import Homework_03 from "./homeworks/Homework_03/Homework_03";
+import Homework_04 from "./homeworks/Homework_05/Homework_05";
 function App() {
   return (
     <> 
@@ -12,7 +13,8 @@ function App() {
        {/* <Homework_02 /> */}
        {/* {<Lesson_03/>} */}
       {/* <Homework_03/>  */}
-          <Lesson_04/>
+          {/* <Lesson_04/> */}
+          <Homework_04/>
     </>
   );
 }
---
+++ src/homeworks/Homework_05/Homework_05.jsx
@@ -0,0 +1,11 @@
+import Feedback from "../../components/Feedback/Feedback"
+<Homework_05 />
+import "./styles.css";
+function Homework_05() {
+  return (
+    <div className="homework_05_page_wrapper">
+      <Feedback />
+    </div>
+  );
+}
+export default Homework_05;
